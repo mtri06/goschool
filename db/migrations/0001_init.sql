82bee2001f45ddc8df63b392c22fcb876942632b
minhtri06 <pmtri.tvv@gmail.com>
2026-04-12 18:31:12 +0700

    feat: move token table to the init migration

--- db/migrations/0001_init.sql
+++ db/migrations/0001_init.sql
@@ -58,6 +58,19 @@ CREATE TABLE class_teachers (
   PRIMARY KEY (teacher_id, class_id, subject_id, year)
 );
 
+CREATE TABLE tokens (
+	id 							SERIAL 			PRIMARY KEY,
+	body 						TEXT 				NOT NULL,
+	user_id 				INT 				NOT NULL,
+	type 						VARCHAR(50) NOT NULL CHECK (type IN ('refresh_token', 'password_update_token', 'email_verification_token')),
+	expires_at 			TIMESTAMPTZ NOT NULL,
+	is_revoked 			BOOLEAN 		NOT NULL DEFAULT FALSE,
+	is_used 				BOOLEAN 		NOT NULL DEFAULT FALSE,
+	is_blacklisted 	BOOLEAN 		NOT NULL DEFAULT FALSE,
+	created_at 			TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at 			TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 ALTER TABLE user_teachers ADD CONSTRAINT fk_user_teachers_user_id FOREIGN KEY (user_id)
 REFERENCES users(id) ON DELETE CASCADE;
 ALTER TABLE user_teachers ADD CONSTRAINT fk_user_teachers_subject_id FOREIGN KEY (subject_id)
@@ -72,8 +85,11 @@ ALTER TABLE class_teachers ADD CONSTRAINT fk_class_teachers_class_id FOREIGN KEY
 REFERENCES class(id) ON DELETE NO ACTION;
 ALTER TABLE class_teachers ADD CONSTRAINT fk_class_teachers_subject_id FOREIGN KEY (subject_id)
 REFERENCES subjects(id) ON DELETE NO ACTION;
+ALTER TABLE tokens ADD CONSTRAINT fk_tokens_user_id FOREIGN KEY (user_id) 
+REFERENCES users(id) ON DELETE CASCADE;
 
 CREATE UNIQUE INDEX idx_users_email ON users (email) WHERE email IS NOT NULL;
+CREATE INDEX idx_tokens_body ON tokens (body);
 
 -- +goose Down
 DROP INDEX IF EXISTS idx_users_email;
@@ -89,3 +105,6 @@ DROP TABLE IF EXISTS subjects;
 DROP TABLE IF EXISTS user_students;
 DROP TABLE IF EXISTS user_teachers;
 DROP TABLE IF EXISTS users;
+DROP INDEX IF EXISTS idx_tokens_body;
+ALTER TABLE tokens DROP CONSTRAINT fk_tokens_user_id;
+DROP TABLE IF EXISTS tokens;
